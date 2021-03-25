bbbb18201a193373b0408d320466ae7ad4b1502b
BoitumeloDikoko <dikokob@gmail.com>
2021-03-25 11:58:23 +0200

    update

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/hello")
+def hello():
+    return "Hello, World!"
+
 @app.route("/")
 def home():
     return render_template("home.html")
